8a7bfe8c7df4aa94f8d0a081c9992f5169a455cc
Arseniy <ylarseniy@gmail.com>
2020-04-07 15:52:22 +0500

    city

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 import logging
 import json
 import random
+import os
 
 app = Flask(__name__)
 
@@ -140,4 +141,5 @@ def get_first_name(req):
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='localhost', port=port)
