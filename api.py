d060131368aa83f1867e424dad2cedb2d87228f2
Arseniy <ylarseniy@gmail.com>
2020-04-07 13:52:57 +0500

    city

--- api.py
+++ api.py
@@ -1,126 +1,145 @@
-# coding: utf-8
-# Импортирует поддержку UTF-8.
-from __future__ import unicode_literals
-
-# Импортируем модули для работы с JSON и логами.
-import os
-import json
-import logging
-
-# Импортируем подмодули Flask для запуска веб-сервиса.
 from flask import Flask, request
+import logging
+import json
+import random
+
 app = Flask(__name__)
 
+logging.basicConfig(level=logging.INFO)
 
-logging.basicConfig(level=logging.DEBUG)
+# создаем словарь, в котором ключ — название города,
+# а значение — массив, где перечислены id картинок,
+# которые мы записали в прошлом пункте.
 
-# Хранилище данных о сессиях.
+cities = {
+    'москва': ['1030494/5a0caa7d6a1cae4efd5b',
+               '1540737/9f695e569779055c1229'],
+    'нью-йорк': ['1533899/e216c3fb2e6b0458e87f',
+                 '1533899/143f08ed28549c84df0f'],
+    'париж': ['1030494/0ffc36c53e49db9e2d70',
+              '965417/92767891fbc01f798de4']
+}
+
+game_over = False
+game_started = False
+right_city = ''
+
+# создаем словарь, где для каждого пользователя
+# мы будем хранить его имя
 sessionStorage = {}
-first = True
 
 
-# Задаем параметры приложения Flask.
-@app.route("/post", methods=['POST'])
+@app.route('/post', methods=['POST'])
 def main():
-    # Функция получает тело запроса и возвращает ответ.
-    logging.info('Request: %r', request.json)
-
+    logging.info(f'Request: {request.json!r}')
     response = {
-        "version": request.json['version'],
-        "session": request.json['session'],
-        "response": {
-            "end_session": False
+        'session': request.json['session'],
+        'version': request.json['version'],
+        'response': {
+            'end_session': False
         }
     }
-
-    handle_dialog(request.json, response)
-
-    logging.info('Response: %r', response)
-
-    return json.dumps(
-        response,
-        ensure_ascii=False,
-        indent=2
-    )
+    handle_dialog(response, request.json)
+    logging.info(f'Response: {response!r}')
+    return json.dumps(response)
 
 
-# Функция для непосредственной обработки диалога.
-def handle_dialog(req, res):
-    global first
+def handle_dialog(res, req):
+    global game_over, game_started, right_city
     user_id = req['session']['user_id']
 
+    # если пользователь новый, то просим его представиться.
     if req['session']['new']:
-        # Это новый пользователь.
-        # Инициализируем сессию и поприветствуем его.
-
+        res['response']['text'] = 'Привет! Назови свое имя!'
+        # создаем словарь в который в будущем положим имя пользователя
         sessionStorage[user_id] = {
-            'suggests': [
-                "Не хочу.",
-                "Не буду.",
-                "Отстань!",
-            ]
+            'first_name': None
         }
-
-        res['response']['text'] = 'Привет! Купи слона!'
-        res['response']['buttons'] = get_suggests(user_id)
         return
 
-    # Обрабатываем ответ пользователя.
-    if req['request']['original_utterance'].lower() in [
-        'ладно',
-        'куплю',
-        'покупаю',
-        'хорошо',
-        'я покупаю',
-        'я куплю'
-    ]:
-        if not first:
-            res['response']['text'] = 'Кролика можно найти на Яндекс.Маркете!'
-            return
-        first = False
-        sessionStorage[user_id]['suggests'] = [
-                "Не хочу.",
-                "Не буду.",
-                "Отстань!",
-            ]
+    # если пользователь не новый, то попадаем сюда.
+    # если поле имени пустое, то это говорит о том,
+    # что пользователь еще не представился.
+    if sessionStorage[user_id]['first_name'] is None:
+        # в последнем его сообщение ищем имя.
+        first_name = get_first_name(req)
+        # если не нашли, то сообщаем пользователю что не расслышали.
+        if first_name is None:
+            res['response']['text'] = \
+                'Не расслышала имя. Повтори, пожалуйста!'
+        # если нашли, то приветствуем пользователя.
+        # И спрашиваем какой город он хочет увидеть.
+        else:
+            sessionStorage[user_id]['first_name'] = first_name
+            res['response'][
+                'text'] = 'Приятно познакомиться, ' \
+                          + first_name.title() \
+                          + '. Я - Алиса. Отгадаешь город по фото?'
 
-        res['response']['text'] = 'А теперь купи кролика!'
-        res['response']['buttons'] = get_suggests(user_id)
-        return
+    else:
+        if not game_over:
+            if not game_started:
+                if req['request']['original_utterance'].lower() in [
+                    'да',
+                    'отгадаю',
+                    'ага',
+                    'го',
+                    'конечно',
+                    'угу',
+                ]:
+                    right_city = random.choice(cities)
+                    res['response']['card'] = {}
+                    res['response']['card']['type'] = 'BigImage'
+                    res['response']['card']['title'] = ''
+                    res['response']['card']['image_id'] = random.choice(cities[right_city])
+                    res['response']['text'] = 'Что это за город?'
+                    game_started = True
+                elif req['request']['original_utterance'].lower() in [
+                    'нет',
+                    'не',
+                    'неа',
+                    'не буду',
+                    'не отгадаю',
+                ]:
+                    game_over = True
+                    game_started = False
+                else:
+                    res['response']['text'] = 'Не расслышала ответ. Попробуй еще разок!'
+            else:
+                # ищем город в сообщение от пользователя
+                city = get_city(req)
+                # если этот город среди известных нам,
+                # то показываем его (выбираем одну из двух картинок случайно)
+                if city in cities:
+                    res['response']['text'] = 'Угадал!'
+                    game_started = False
+                # если не нашел, то отвечает пользователю
+                # 'Первый раз слышу об этом городе.'
+                else:
+                    res['response']['text'] = \
+                        'Не угадал. Попробуй еще разок!'
 
-    # Если нет, то убеждаем его купить слона!
-    res['response']['text'] = 'Все говорят "%s", а ты купи %s!' % (
-        req['request']['original_utterance'], 'слона' if first else 'кролика'
-    )
-    res['response']['buttons'] = get_suggests(user_id)
+
+def get_city(req):
+    # перебираем именованные сущности
+    for entity in req['request']['nlu']['entities']:
+        # если тип YANDEX.GEO то пытаемся получить город(city),
+        # если нет, то возвращаем None
+        if entity['type'] == 'YANDEX.GEO':
+            # возвращаем None, если не нашли сущности с типом YANDEX.GEO
+            return entity['value'].get('city', None)
 
 
-# Функция возвращает две подсказки для ответа.
-def get_suggests(user_id):
-    session = sessionStorage[user_id]
-
-    # Выбираем две первые подсказки из массива.
-    suggests = [
-        {'title': suggest, 'hide': True}
-        for suggest in session['suggests'][:2]
-    ]
-
-    # Убираем первую подсказку, чтобы подсказки менялись каждый раз.
-    session['suggests'] = session['suggests'][1:]
-    sessionStorage[user_id] = session
-
-    # Если осталась только одна подсказка, предлагаем подсказку
-    # со ссылкой на Яндекс.Маркет.
-    if len(suggests) < 2:
-        suggests.append({
-            "title": "Ладно",
-            "url": f"https://market.yandex.ru/search?text={'слон' if first else 'кролик'}",
-            "hide": True
-        })
-
-    return suggests
+def get_first_name(req):
+    # перебираем сущности
+    for entity in req['request']['nlu']['entities']:
+        # находим сущность с типом 'YANDEX.FIO'
+        if entity['type'] == 'YANDEX.FIO':
+            # Если есть сущность с ключом 'first_name',
+            # то возвращаем ее значение.
+            # Во всех остальных случаях возвращаем None.
+            return entity['value'].get('first_name', None)
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='localhost', port=port)
+    app.run()
